906f1a35267f69e6b61889ae4eee5c915f272bf2
TWalk007 <walkertb03@gmail.com>
2017-07-14 13:29:40 -0400

    Check-in
    First build of the touch iphone version.

--- Assets/_Scripts/ToolChangerHotspotTouch.cs
+++ Assets/_Scripts/ToolChangerHotspotTouch.cs
@@ -4,33 +4,12 @@ using UnityEngine;
 
 public class ToolChangerHotspotTouch : MonoBehaviour {
 
-	public float gazeActivationTime = 1.25f;
 	public ExploreControllerTouch exploreControllerTouch;
 
-	private float timeElapsed;
-	private bool isEntered = false;
 
-	void Update () {
-		if (isEntered) {
-			ActivateTC ();
-		}
-	}
-
-	void ActivateTC(){
-		timeElapsed += Time.deltaTime;
-		if (timeElapsed >= gazeActivationTime) {
-			timeElapsed = 0;
-			exploreControllerTouch.myState = ExploreControllerTouch.States.toolChanger;
-		}
-	}
-
-	public void OnPointerEnter (){
-		isEntered = true;
-	}
-
-	public void OnPointerExit (){
-		isEntered = false;
-		timeElapsed = 0;
+	private void OnMouseDown()
+    {
+		exploreControllerTouch.myState = ExploreControllerTouch.States.toolChanger;
 	}
 
 }
