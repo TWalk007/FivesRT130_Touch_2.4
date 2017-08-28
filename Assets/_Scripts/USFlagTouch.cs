6545183adca27d65ba518185f5061c32d3794995
TWalk007 <walkertb03@gmail.com>
2017-08-28 15:30:51 -0400

    Fixed US Flag
    Also decreased look sensitivity.

--- Assets/_Scripts/USFlagTouch.cs
+++ Assets/_Scripts/USFlagTouch.cs
@@ -4,39 +4,12 @@ using UnityEngine;
 
 public class USFlagTouch : MonoBehaviour {
 
-	public float gazeActivationTime = 1.25f;
 	public ExploreControllerTouch exploreController;
 	public ExploreSceneAudio exploreAudio;
 
-	private float timeElapsed;
-	private bool isEntered = false;
-
-	void Update () {
-		if (isEntered) {
-			USFlagVO ();
-		} else {
-			timeElapsed = 0;
-		}
-	}
-
-	public void USFlagVO(){
-		timeElapsed += Time.deltaTime;
-
-		if (timeElapsed >= gazeActivationTime) {
-			timeElapsed = 0;
-			exploreAudio.selectedClip = exploreAudio.audioClip [5];
-			exploreController.myState = ExploreControllerTouch.States.usFlag;
-			isEntered = false;
-		}
-
-	}
-
-	public void OnPointerEnter (){
-		isEntered = true;
-	}
-
-	public void OnPointerExit (){
-		isEntered = false;
-		timeElapsed = 0;
-	}
+    private void OnMouseDown()
+    {
+        exploreAudio.selectedClip = exploreAudio.audioClip[5];
+        exploreController.myState = ExploreControllerTouch.States.usFlag;
+    }    
 }
